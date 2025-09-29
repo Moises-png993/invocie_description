bbe13469ce05ad187498439e6eb03cf90fcbe48c
Moises-png993 <pr20028@ues.edu.sv>
2025-09-29 12:31:31 -0600

    update models to simpex

--- simpex/models.py
+++ simpex/models.py
@@ -5,7 +5,7 @@ class Contenedor(models.Model):
     embarque = models.CharField(max_length=100)
     expediente = models.CharField(max_length=50, unique=True)
     tamano = models.CharField(max_length=20)  # Ej: "20 ft", "40 ft"
-    fecha_pos = models.DateField()
+    fecha_pos = models.DateField(null=True, blank=True)
     fecha_despacho = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=50, choices=[
         ("EN_TRANSITO", "En tránsito"),
@@ -13,9 +13,9 @@ class Contenedor(models.Model):
         ("ENTREGADO", "Entregado"),
     ])
     transporte = models.CharField(max_length=50)  # Ej: "MSC", "Maersk"
-    eta = models.DateField()
-    dai = models.DecimalField(max_digits=12, decimal_places=2)
-    fts = models.DecimalField(max_digits=12, decimal_places=2)
+    eta = models.DateField(null=True, blank=True)
+    dai = models.CharField(max_length=40)
+    fts = models.CharField(max_length=40)
 
     def __str__(self):
         return f"{self.embarque} - {self.expediente}"
