440d3051d8cc653e9cf49f77e478584adb6af6ad
Moises-png993 <pr20028@ues.edu.sv>
2025-09-29 11:53:15 -0600

    Update Cache V2

--- static/service-worker.js
+++ static/service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open('impex-cache').then(cache => {
+    caches.open('impex-cache-v3').then(cache => {
       return cache.match(event.request).then(response => {
         return response || fetch(event.request).then(networkResponse => {
           cache.put(event.request, networkResponse.clone());
